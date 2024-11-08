cb61174db160283246f9493a826692ce19bfcd14
Anna Nahalka <nahalkaanna06@gmail.com>
2024-11-08 13:39:31 +0200

    Splash Screen

---
+++ PrepWise/app/src/main/java/com/example/prepwise/activities/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.prepwise.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.prepwise.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000) // затримка в 2 секунди
+    }
+}
